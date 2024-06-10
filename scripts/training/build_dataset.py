648c2f588bfe50f399d3543dea281bfffed8d884
WolverineX23 <547036318@qq.com>
2024-06-10 11:19:37 +0800

    formatted

--- scripts/training/build_dataset.py
+++ scripts/training/build_dataset.py
@@ -7,62 +7,60 @@ import torch
 from datasets import load_dataset, concatenate_datasets
 import transformers
 
-
 IGNORE_INDEX = -100
 
 logger = logging.getLogger('__name__')
 
 PROMPT_TEMPLATE = (
-        "[INST] <<SYS>>\n"
-        "You are a helpful assistant. 你是一个乐于助人的助手。\n"
-        "<</SYS>>\n\n{instruction} [/INST]"
-    )
+    "[INST] <<SYS>>\n"
+    "You are a helpful assistant. 你是一个乐于助人的助手。\n"
+    "<</SYS>>\n\n{instruction} [/INST]"
+)
 
-def build_instruction_dataset(data_path: Union[List[str],str],
-                tokenizer: transformers.PreTrainedTokenizer,
-                max_seq_length: int, data_cache_dir = None,
-                preprocessing_num_workers = None,
-                ):
 
+def build_instruction_dataset(data_path: Union[List[str], str],
+                              tokenizer: transformers.PreTrainedTokenizer,
+                              max_seq_length: int, data_cache_dir=None,
+                              preprocessing_num_workers=None,
+                              ):
     def tokenization(examples):
         sources = []
         targets = []
         prompt = PROMPT_TEMPLATE
-        for instruction, input, output in zip(examples['instruction'],examples['input'],examples['output']):
-            if input is not None and input !="":
-                instruction = instruction+'\n'+input
-            source = prompt.format_map({'instruction':instruction})
+        for instruction, input, output in zip(examples['instruction'], examples['input'], examples['output']):
+            if input is not None and input != "":
+                instruction = instruction + '\n' + input
+            source = prompt.format_map({'instruction': instruction})
             target = f"{output}{tokenizer.eos_token}"
 
             sources.append(source)
             targets.append(target)
 
-        tokenized_sources = tokenizer(sources,return_attention_mask=False)
-        tokenized_targets = tokenizer(targets,return_attention_mask=False,add_special_tokens=False)
+        tokenized_sources = tokenizer(sources, return_attention_mask=False)
+        tokenized_targets = tokenizer(targets, return_attention_mask=False, add_special_tokens=False)
 
         all_input_ids = []
         all_labels = []
-        for s,t in zip(tokenized_sources['input_ids'],tokenized_targets['input_ids']):
+        for s, t in zip(tokenized_sources['input_ids'], tokenized_targets['input_ids']):
             input_ids = torch.LongTensor(s + t)[:max_seq_length]
             labels = torch.LongTensor([IGNORE_INDEX] * len(s) + t)[:max_seq_length]
             assert len(input_ids) == len(labels)
             all_input_ids.append(input_ids)
             all_labels.append(labels)
 
-        results = {'input_ids':all_input_ids, 'labels': all_labels}
+        results = {'input_ids': all_input_ids, 'labels': all_labels}
         return results
-
 
     logging.warning("building dataset...")
     all_datasets = []
 
-    if not isinstance(data_path,(list,tuple)):
+    if not isinstance(data_path, (list, tuple)):
         data_path = [data_path]
     for file in data_path:
 
         if data_cache_dir is None:
             data_cache_dir = str(os.path.dirname(file))
-        cache_path = os.path.join(data_cache_dir,os.path.basename(file).split('.')[0]+f"_{max_seq_length}")
+        cache_path = os.path.join(data_cache_dir, os.path.basename(file).split('.')[0] + f"_{max_seq_length}")
         os.makedirs(cache_path, exist_ok=True)
         try:
             processed_dataset = datasets.load_from_disk(cache_path)
@@ -74,7 +72,7 @@ def build_instruction_dataset(data_path: Union[List[str],str],
                 tokenization_func,
                 batched=True,
                 num_proc=preprocessing_num_workers,
-                remove_columns=["instruction","input","output"],
+                remove_columns=["instruction", "input", "output"],
                 keep_in_memory=False,
                 desc="preprocessing on dataset",
             )
@@ -84,6 +82,7 @@ def build_instruction_dataset(data_path: Union[List[str],str],
         all_datasets.append(processed_dataset['train'])
     all_datasets = concatenate_datasets(all_datasets)
     return all_datasets
+
 
 @dataclass
 class DataCollatorForSupervisedDataset(object):
